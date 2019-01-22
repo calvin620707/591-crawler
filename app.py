a0185384fb59b359b7c6f1f055cc2d7f59e84974
Calvin Wu <calvin620707@msn.com>
2019-01-22 16:45:05 +0800

    basic support for housefun

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
 import os
-from pathlib import Path
 import pickle
+import subprocess
+from pathlib import Path
+from urllib.parse import urlparse
 
 from flask import Flask, render_template, request
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
-import subprocess
 
 output = Path('./output')
 
 app = Flask(__name__)
 
+def decide_spider(url):
+    domains = {
+        'buy.housefun.com.tw': 'housefun',
+        'sale.591.com.tw': '591',
+        'www.591.com.tw': '591',
+    }
+
+    try:
+        return domains[urlparse(url).netloc]
+    except KeyError:
+        return None
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/591', methods=['POST'])
+@app.route('/crawl', methods=['POST'])
 def crawl_591():
     if request.method == 'POST':
         url = request.form["url"]
-        if 'sale.591.com.tw' not in url and 'www.591.com.tw' not in url:
-            return "請輸入591網址"
+        spider = decide_spider(url)
+        if not spider:
+            return "目前只支援591跟好房網"
 
         if output.exists():
             os.remove(str(output))
         subprocess.run(
-            ['scrapy', 'crawl', '591', '-a', f'url={url}'],
+            ['scrapy', 'crawl', spider, '-a', f'url={url}'],
         )
         with output.open('rb') as f:
             data = pickle.load(f)
         data['url'] = url
-        return render_template('591_results.html', data=data)
+        return render_template('results.html', data=data)
---
+++ crawlers/spiders/housefun.py
@@ -0,0 +1,56 @@
+import scrapy
+
+class Spider(scrapy.Spider):
+    name = 'housefun'
+
+    def start_requests(self):
+        if not hasattr(self, 'url'):
+            raise RuntimeError('Please input url with -a url=<url>')
+        yield scrapy.Request(getattr(self, 'url'), self.parse)
+
+    def parse(self, response):
+        self.extra_info = response.css('.extra-info span.desc::text')
+        self.info_sub = response.css('.info-sub>li::text')
+        address = self._get_extra_info(0)
+        floor = self._get_extra_info(1).split('~')[1].strip()
+        locate_floor, max_floor = floor.split('/')
+
+        size_info = self._get_info_sub(1)
+        total_size = size_info.split('(')[0].strip('總坪數 坪')
+
+        yield {
+            'title': response.css('.main>.heading::text').extract_first(),
+            'house_type': self._get_extra_info(4).split('/')[0],
+            'district': address[3:6],
+            'years': self._find_extra_info('屋齡').strip('屋齡年 '),
+            'structure': self._get_info_sub(0),
+            'total_price': response.css('#total-amount::text').extract_first().replace(',', ''),
+            'price_per': self._get_info_sub(2).strip('單價萬/坪 單價含車位單價計算方式請洽業務'),
+            'total_size': total_size,
+            'locate_floor': locate_floor,
+            'max_floor': max_floor,
+            'main_size': response.css('.popover-content .info::text')[0].extract().strip(' 坪'),
+            'public_ratio': '',
+            'manage_fee': self._find_extra_info('管理費').strip('管理費 元/月').replace(',', ''),
+            'parking': self._find_extra_info('車位'),
+            'address': address,
+        }
+
+    def _get_extra_info(self, index):
+        try:
+            return self.extra_info[index].extract()
+        except:
+            return ''
+
+    def _find_extra_info(self, keyword):
+        for item in self.extra_info:
+            text = item.extract()
+            if keyword in text:
+                return text
+        return ''
+
+    def _get_info_sub(self, index):
+        try:
+            return self.info_sub[index].extract()
+        except:
+            return ''
